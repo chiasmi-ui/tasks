19833853d27cefbf1c6cff668f317b0c245165f4
chiasmi-ui <chiasmi@udel.edu>
2026-08-28 14:13:04 -0400

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ function App(): React.JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Asmi Chinauriya
+                Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. 
             </p>
         </div>
     );

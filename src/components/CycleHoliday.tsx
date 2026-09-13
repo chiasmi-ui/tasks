071af0afab7ce71eb08c7270fe7d7f4993dd1839
Your Full Name <yourname@udel.edu>
2026-09-13 16:33:51 -0400

    finished assignment

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "New Year's Day"
+    | "Easter"
+    | "Independence Day"
+    | "Halloween"
+    | "Christmas";
+
+const HOLIDAY_EMOJI: Record<Holiday, string> = {
+    "New Year's Day": "🎆",
+    Easter: "🐰",
+    "Independence Day": "🎇",
+    Halloween: "🎃",
+    Christmas: "🎄"
+};
+
+const ALPHABET_ORDER: Record<Holiday, Holiday> = {
+    Christmas: "Easter",
+    Easter: "Halloween",
+    Halloween: "Independence Day",
+    "Independence Day": "New Year's Day",
+    "New Year's Day": "Christmas"
+};
+
+const YEAR_ORDER: Record<Holiday, Holiday> = {
+    "New Year's Day": "Easter",
+    Easter: "Independence Day",
+    "Independence Day": "Halloween",
+    Halloween: "Christmas",
+    Christmas: "New Year's Day"
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Christmas");
+
+    return (
+        <div>
+            <div>Holiday: {HOLIDAY_EMOJI[holiday]}</div>
+            <Button onClick={() => setHoliday(ALPHABET_ORDER[holiday])}>
+                Advance by Alphabet
+            </Button>
+            <Button onClick={() => setHoliday(YEAR_ORDER[holiday])}>
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,25 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    return <div>
+        <Button
+                disabled={inProgress || attempts === 0}
+                onClick={() => {
+                    setInProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+            >
+                Start Quiz
+            </Button>
+            <Button disabled={!inProgress} onClick={() => setInProgress(false)}>
+                Stop Quiz
+            </Button>
+            <Button disabled={inProgress} onClick={() => setAttempts(attempts + 1)}>
+                Mulligan
+            </Button>
+            <div>Attempts: {attempts}</div>
+        </div>;
 }
